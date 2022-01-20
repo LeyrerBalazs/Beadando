79a002d36b05f597394d20a3cc5f4a69c2a42a33
LeyrerBalazs <90619307+LeyrerBalazs@users.noreply.github.com>
2022-01-20 12:43:19 +0100

    Update writes.py

--- writes.py
+++ writes.py
@@ -1,28 +1,36 @@
-import os, json
+import os, json, main
 
 def Behuzas():
-	print("                  ", end="")
+        """
+                Ez a függvénny a szebb kódért felelős, illetve a terminálon való igazításért.
+        """
+        print("                  ", end="")
 
 def Menu():
-	os.system("clear")
-	print("\n\n")
-	Behuzas()
-	print("\u001b[42m                               \u001b[0m")
-	Behuzas()
-	print("\u001b[42m  \u001b[0m                           \u001b[42m  \u001b[0m")
-	Behuzas()
-	print("\u001b[42m  \u001b[0m       \u001b[37m1. Jatek            \u001b[42m  \u001b[0m")
-	Behuzas()
-	print("\u001b[42m  \u001b[0m       \u001b[37m2. Pontok           \u001b[42m  \u001b[0m")
-	Behuzas()
-	print("\u001b[42m  \u001b[0m       \u001b[37m3. Kilépés          \u001b[42m  \u001b[0m")
-	Behuzas()
-	print("\u001b[42m  \u001b[0m                           \u001b[42m  \u001b[0m")
-	Behuzas()
-	print("\u001b[42m                               \u001b[0m")
-	print("")
-	
+        """
+                Kiíratja a menűt.
+        """
+        os.system("clear")
+        print("\n\n")
+        Behuzas()
+        print("\u001b[42m                               \u001b[0m")
+        Behuzas()
+        print("\u001b[42m  \u001b[0m                           \u001b[42m  \u001b[0m")
+        Behuzas()
+        print("\u001b[42m  \u001b[0m       \u001b[37m1. Jatek            \u001b[42m  \u001b[0m")
+        Behuzas()
+        print("\u001b[42m  \u001b[0m       \u001b[37m2. Pontok           \u001b[42m  \u001b[0m")
+        Behuzas()
+        print("\u001b[42m  \u001b[0m       \u001b[37m3. Kilépés          \u001b[42m  \u001b[0m")
+        Behuzas()
+        print("\u001b[42m  \u001b[0m                           \u001b[42m  \u001b[0m")
+        Behuzas()
+        print("\u001b[42m                               \u001b[0m")
+
 def Stats():
+        """
+                Felelős az eredmények megjelenítéséért és így a fájból való olvasásért is.
+        """
         os.system("clear")
         print("\n\n")
         Behuzas()
@@ -47,10 +55,29 @@ def Stats():
         print("")
         Behuzas()
         input("Tovább...")
-        os.system("python3 main.py")
-        exit()
+        main.main()
 
+def WriteInFile(pont:int, name:str):
+        """
+                Ez felelős eldönteni van e ilyen játékos már és módosítani vagy hozzáfűzni az adatokat.
+        """
+        van = False
+        with open("score.json", encoding="utf-8") as f:
+                data = json.load(f)
+        for i in range(len(data)):
+                if data[i]["nev"] == name:
+                        van = True
+                        data[i]["pontja"] = pont
+        if van == False:
+                newuser = {"nev":name, "pontja": pont}
+                data.append(newuser)
+        with open("score.json", "w",  encoding="utf-8") as f:
+    	        json.dump(data, f)
+        
 def ShipEnd(pont:int, name:str):
+        """
+                Ship end függvény felelős kiiratni az eredményt majd elmenteni is a score.json fileba. Illetve hogy megvizsgálja és felülírja ha már az adott játékos játszott.
+        """
         if pont < 10:
                 kiirott = "000" + str(pont)
         elif pont < 100:
@@ -75,17 +102,5 @@ def ShipEnd(pont:int, name:str):
         print("\u001b[42m                                                    \u001b[0m")
         Behuzas()
         input("Tovább...")
-        van = False
-        os.system("python3 main.py")
-        with open("score.json", encoding="utf-8") as f:
-                data = json.load(f)
-        for i in range(len(data)):
-                if data[i]["nev"] == name:
-                        van = True
-                        data[i]["pontja"] = pont
-        if van == False:
-                newuser = {"nev":name, "pontja": pont}
-                data.append(newuser)
-        with open("score.json", "w",  encoding="utf-8") as f:
-    		json.dump(data, f)
-        exit()
+        WriteInFile(pont, name)
+        main.main()
