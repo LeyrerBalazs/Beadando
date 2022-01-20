80ff001942d4eb3adf0d2d466e52c082d8a07e3e
LeyrerBalazs <90619307+LeyrerBalazs@users.noreply.github.com>
2022-01-20 12:42:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 import writes, os, ship
 
 def main():
-        writes.Menu()
-        writes.Behuzas()
-        valasz = input("A választás száma: ")
-        length = 100
-        elso = True
-        if valasz == "1":
-                while length > 9:
-                        if elso:
-                                elso =  False
-                        else:
+        while True:
+                writes.Menu()
+                writes.Behuzas()
+                valasz = input("A választás száma: ")
+                length = 100
+                elso = True
+                if valasz == "1":
+                        while length > 9:
+                                if elso:
+                                        elso =  False
+                                else:
+                                        writes.Behuzas()
+                                        print("Túl hosszú név!")
                                 writes.Behuzas()
-                                print("Túl hosszú név!")
-                        writes.Behuzas()
-                        name = input("Add meg a neved!: ")
-                        length = len(name)
-                ship.Game(name)
-        elif valasz == "2":
-                writes.Stats()
-        elif valasz == "3":
-                os.system("clear")
-                exit()
-        else:
-                main()
+                                name = input("Add meg a neved!: ")
+                                length = len(name)
+                        ship.Game(name)
+                elif valasz == "2":
+                        writes.Stats()
+                elif valasz == "3":
+                        os.system("clear")
 
 if __name__ == "__main__":
         main()
